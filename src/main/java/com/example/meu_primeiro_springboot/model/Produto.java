30ef40dd18999bab0105df8c2107233bb6c02cca
Francis <irenofrancinato@gmail.com>
2025-10-14 18:30:41 -0300

    Subindo os estudos Spring para o github

---
+++ src/main/java/com/example/meu_primeiro_springboot/model/Produto.java
@@ -0,0 +1,43 @@
+package com.example.meu_primeiro_springboot.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private double preco;
+
+    public Produto() {}
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return this.preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+}
